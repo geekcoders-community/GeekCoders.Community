a9c84006961eddb32917dd2a70eab26e3e92f62d
AmalFrancisOlakengil <francisamal030@gmail.com>
2024-10-17 00:54:27 +0530

    Update login.js

--- app/login/login.js
+++ app/login/login.js
@@ -3,7 +3,7 @@ export default function Login(){
 
 return(<>
 <div className="flex min-h-screen bg-gray-100">
-     {/* Red Box Section - Right Side */}
+     {/* Red Box Section - left Side */}
      <div className="hidden md:flex pl-5 flex-1 items-center justify-center">
      <div className="bg-[url('./assets/loginframe.png')] bg-cover bg-center text-white pl-20 px-8 py-20 rounded-3xl w-3/4 h-5/6">
           <h1 className="text-4xl font-bold mt-32">Welcome back to</h1>
@@ -12,17 +12,17 @@ return(<>
       </div>
 
 
-      {/* Sign Up Form Section */}
+      {/* Sign in Form Section */}
       <div className="flex flex-1 items-center justify-center p-6">
-        {/* Container for the Sign-Up Form */}
+        {/* Container for the Sign-in Form */}
         <div className="max-w-md w-full">
-          {/* Header for the Sign-Up Form */}
+          {/* Header for the Sign-in Form */}
           <h2 className="text-3xl font-Clashd font-bold mb-4">Login</h2>
           <p className="text-gray-600 mb-4">
             Welcome back!<br />Please enter your credentials
           </p>
 
-          {/* Sign-Up Form */}
+          {/* Sign-in Form */}
           <form>
             {/* Name Input Field */}
             <div className="mb-4">
@@ -45,7 +45,7 @@ return(<>
                 className="mt-1 w-full px-3 py-2 border rounded-md bg-gray-200 focus:outline-none"
               />
             </div>
-            
+            {/*checkbox section*/}
             <div className="flex items-center">
             <input
                 type="checkbox"
@@ -68,9 +68,8 @@ return(<>
           <button
               type="submit"
               className="w-full text-gray-400 py-2 rounded-md text-sm font-semibold border-2 border-red-600"
-            >
-              Continue using Google</button>
-          {/* Link to Login Page */}
+            >Continue using Google</button>
+              {/* Link to sign up Page */}
           <p className="mt-4 text-center text-sm text-gray-600">
             Don't have an account? <a href="#" className="text-red-600 hover:underline">Sign Up</a>
           </p>
