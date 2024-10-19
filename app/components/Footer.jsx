a122cb3468c4bc375b37a74d71739727639fc437
badrinarayanms <badrinarayan.moola@gmail.com>
2024-10-20 05:02:48 +0530

    home page

---
+++ app/components/Footer.jsx
@@ -0,0 +1,74 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image';
+import logo from '../assets/logo.png';
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareInstagram ,FaDiscord} from "react-icons/fa6";
+const Footer = () => {
+  return (
+    <div className='flex justify-center items-center'>
+    <div className='  flex flex-col justify-between items-center w-full bg-[#191A23] rounded-t-3xl p-10'>
+
+    <div className='flex justify-between w-full items-center '>
+        <div className="flex items-center space-x-2">
+            {/* <Image src={logo} className='h-14 w-auto' /> */}
+            <Link href="/login"><span className="font-bold text-lg text-[rgb(220,20,60)]">Geek Coders</span></Link>
+          </div>
+
+        <div className="hidden md:flex space-x-8">
+                    <Link href="/">
+                    <p className="text-white font-clash hover:text-red-500">Home</p>
+                    </Link>
+                    <Link href="/about">
+                    <p className="text-white font-clash  hover:text-red-500">About us</p>
+                    </Link>
+                    <Link href="/events">
+                    <p className="text-white font-clash  hover:text-red-500">Events</p>
+                    </Link>
+                    <Link href="/faq">
+                    <p className="text-white  font-clash  hover:text-red-500">Faq</p>
+                    </Link>
+                    <Link href="/contact">
+                    <p className="text-white font-clash  hover:text-red-500">Contact</p>
+                    </Link>
+                </div>
+                    
+                <div className="flex gap-3">
+                    <Link href="/https://www.linkedin.com/company/geekcoderscommunity"><FaLinkedin size={30} color='white'/></Link>
+                    <Link href="/https://www.instagram.com/geekcoders.community/"><FaSquareInstagram size={30} color='white'/></Link>
+                    <Link href="/join"><FaDiscord size={30} color='white' /></Link>
+                </div>
+            </div>
+            <div
+          className={`md:hidden  `}
+        > 
+          <div className="mt-4 mx-auto flex justify-between w-full items-center gap-5 ">
+            <Link href="/">
+              <p className="block text-white text-sm  hover:text-red-500">Home</p>
+            </Link>
+            <Link href="/about">
+              <p className="block text-white text-sm hover:text-red-500">About us</p>
+            </Link>
+            <Link href="/events">
+              <p className="block text-white text-sm hover:text-red-500">Events</p>
+            </Link>
+            <Link href="/faq">
+              <p className="block text-white text-sm hover:text-red-500">Faq</p>
+            </Link>
+            <Link href="/contact" >
+              <p className="block text-white text-sm hover:text-red-500 ">Contact</p>
+            </Link>
+           
+          </div>
+          </div>
+            {/* <div className="w-full border-t-2 border-white "></div> */}
+            <div className="mt-8 border-t border-gray-700 w-full pt-4">
+    <p className="text-center text-xs text-gray-400">© 2024 [Community Name]. All rights reserved. | <a href="/privacy" className="hover:underline">Privacy Policy</a> | <a href="/terms" className="hover:underline">Terms of Service</a></p>
+  </div>
+</div>
+</div>
+
+  )
+}
+
+export default Footer
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,103 @@
+"use client";
+import React, { useState } from 'react';
+import Link from 'next/link';
+
+import Image from 'next/image';
+import logo from '../assets/logo.png';  
+export default function Navbar () {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
+  
+    return (
+      <nav className="bg-white p-4 shadow-md w-full absolute">
+        <div className="container mx-auto flex justify-between items-center">
+          {/* Logo */}
+          <div className="flex items-center space-x-2">
+            <Image src={logo} className='h-14 w-auto' />
+            <Link href="/login"><span className="font-semibold text-lg font-clash ">Geek Coders</span></Link>
+            
+          </div>
+  
+          {/* Hamburger Button for Mobile */}
+          <div className="md:hidden">
+            <button onClick={toggleMenu} className="text-gray-800 focus:outline-none">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
+                ></path>
+              </svg>
+            </button>
+          </div>
+  
+          {/* Links for Desktop */}
+          <div className="hidden md:flex space-x-8">
+            <Link href="/">
+              <p className="text-gray-800 font-medium  font-clash hover:text-red-500">Home</p>
+            </Link>
+            <Link href="/about">
+              <p className="text-gray-800 font-medium  font-clash hover:text-red-500">About us</p>
+            </Link>
+            <Link href="/events">
+              <p className="text-gray-800 font-medium  font-clash hover:text-red-500">Events</p>
+            </Link>
+            <Link href="/faq">
+              <p className="text-gray-800 font-medium  font-clash hover:text-red-500">Faq</p>
+            </Link>
+            <Link href="/contact">
+              <p className="text-gray-800 font-medium  font-clash hover:text-red-500">Contact</p>
+            </Link>
+          </div>
+  
+          {/* Join Button */}
+          <div className="hidden md:block ">
+            <Link href="/join">
+              <p className="bg-[#DC143C]   font-semibold  font-clash text-white py-2 px-6 rounded-md hover:bg-red-600">
+                Join GC
+              </p>
+            </Link>
+          </div>
+        </div>
+  
+        {/* Mobile Menu with Animation */}
+        <div
+          className={`md:hidden overflow-hidden transition-all duration-300  ${isOpen ? 'ease-in max-h-96 opacity-100' : 'ease-in max-h-0 opacity-0'}`}
+        > 
+          <div className="mt-4 space-y-4 ">
+            <Link href="/">
+              <p className="block text-gray-800 hover:text-red-500">Home</p>
+            </Link>
+            <Link href="/about">
+              <p className="block text-gray-800 hover:text-red-500">About us</p>
+            </Link>
+            <Link href="/events">
+              <p className="block text-gray-800 hover:text-red-500">Events</p>
+            </Link>
+            <Link href="/faq">
+              <p className="block text-gray-800 hover:text-red-500">Faq</p>
+            </Link>
+            <Link href="/contact" >
+              <p className="block text-gray-800 hover:text-red-500 mb-4">Contact</p>
+            </Link>
+            <Link href="/join">
+              <p className="block bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600">
+                Join GC
+              </p>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    );
+  };
+  
